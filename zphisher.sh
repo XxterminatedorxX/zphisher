b5095ab516292d4467edbbc43e2b353da8b1d2bf
Israel <74831223+XxterminatedorxX@users.noreply.github.com>
2022-04-05 19:39:14 -0500

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ##   Zphisher 	: 	Automated Phishing Tool
-##   Author 	: 	TAHMID RAYAT 
+##   Author 	: 	XxterminatedorxX 
 ##   Version 	: 	2.2
-##   Github 	: 	https://github.com/htr-tech
+##   Github 	: 	https://github.com/XxterminatedorxX/zphisher
 
 ##   THANKS TO :
 ##   Aditya Shakya - https://github.com/adi1090x
